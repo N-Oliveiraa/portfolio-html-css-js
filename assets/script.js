d89c9eed2c6cd500fdc5bd87724bc0c8cbc11080
N-Oliveiraa <nivaldofdc2@gmail.com>
2026-04-15 11:59:19 -0300

    corrige caminhos das imagens dos projetos

--- assets/script.js
+++ assets/script.js
@@ -91,21 +91,21 @@ const meusProjetos = [
     {
         titulo: "Projeto 01 - Viagem pela vila",
         descricao: "Uma langpage feita com HTML, CSS e JavaScript.",   
-        imagem: "/assets/imagens/projeto1.png",
+        imagem: "assets/imagens/projeto1.png",
         link: "https://n-oliveiraa.github.io/viagens--landing-page/",
         github: "https://github.com/N-Oliveiraa/viagens--landing-page.git"
     },
     {
         titulo: "Projeto 02 - Contador de cliques",
         descricao: "Projeto feito com Vite + React.", 
-        imagem: "/assets/imagens/projeto2.png",
+        imagem: "assets/imagens/projeto2.png",
         link: "https://n-oliveiraa.github.io/contador-de-cliques/",
         github: "https://github.com/N-Oliveiraa/contador-de-cliques.git"
     },
     {
         titulo: "Projeto 03 - Lista de tarefas",
         descricao: "Projeto feito em React com Context API e localStorage.",   
-        imagem: "/assets/imagens/projeto3.png",
+        imagem: "assets/imagens/projeto3.png",
         link: "https://n-oliveiraa.github.io/aplicacao-de-tarefas/",
         github: "https://github.com/N-Oliveiraa/aplicacao-de-tarefas.git",
     }
